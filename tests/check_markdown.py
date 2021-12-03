1942c61ac853e2804b150b1af6b14078b503820b
Tim Lamborn <tlamborn@gmail.com>
2021-12-04 01:13:22 +1300

    Implemented ignore rules for markdown
    Added launch.json for markdown tests

--- tests/check_markdown.py
+++ tests/check_markdown.py
@@ -5,12 +5,35 @@ from sys import call_tracing
 LINK_REGEX = re.compile(r'\[(.*?)\]\((.*?)\)')
 ANCHOR_REGEX = re.compile(r'\s*\#+\s*(.*)')
 
+def parse_ignore_file(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding="utf8") as f:
+            for line in f.readlines():
+                key = line.split("//")[0].strip()
+                if key:
+                    yield key
+
+IGNORE_URIS = [k for k in parse_ignore_file("tests/uri.ignore")]
+
+TEST_OK = 0
+TEST_WARN = 1
+TEST_ERROR = 2
+
+TEST_EMOJI = {
+    TEST_OK: "✔️ ",
+    TEST_WARN: "⚠️ ",
+    TEST_ERROR: "❌ "
+}
+
 # Checks if the markdown URI is valid
 # Returns a tuple of (valid, comment)
 def check_link(uri, source_path):
 
+    if uri in IGNORE_URIS:
+        return TEST_WARN, "URI in ignore list."
+
     if uri.startswith("https"):
-        return True, "http link assumed valid."
+        return TEST_OK, "Web url assumed valid."
     
     # Split out the file and the anchor
     anchor_tokens = uri.count('#')
@@ -19,7 +42,7 @@ def check_link(uri, source_path):
     elif anchor_tokens == 1:
         path, anchor = uri.split('#')
     else:
-        return False, "Too many '#' in link."
+        return TEST_ERROR, "Too many '#' in uri."
 
     if path == "":
         # An unspecified file is the current file
@@ -27,25 +50,25 @@ def check_link(uri, source_path):
     else:
         # It must point to a markdown file
         if not path.endswith(".md"):
-            return False, "File must end with .md"
+            return TEST_ERROR, "File must end with .md"
 
         # Otherwise it is relative to the current file
         path = os.path.normpath(os.path.join(os.path.dirname(source_path), path))
 
         # It must exist
         if not os.path.exists(path):
-            return False, "File does not exist"
+            return TEST_ERROR, "File does not exist."
 
     # If there is an anchor, it must exist
     if anchor is not None:
         anchor = anchor.replace('-', ' ').lower()
         for candidate in all_anchors(path):
             if anchor == candidate.lower():
-                return True, ""
-        return False, "Anchor does not exist"
+                return TEST_OK, ""
+        return TEST_ERROR, "Anchor does not exist."
 
     # Looks fine so far
-    return True, "No anchor"
+    return TEST_OK, "No anchor."
 
 # Iterates over all anchors in a markdown file
 def all_anchors(path):
@@ -54,10 +77,6 @@ def all_anchors(path):
             match = ANCHOR_REGEX.match(line)
             if match:
                 yield match.group(1).strip()
-
-# Returns a string of the emoji to use for a test result
-def pass_emoji(passed):
-    return "✔️ " if passed else "❌ "
 
 # Checks the contents of a markdown file for links
 def check_links(path):
@@ -69,10 +88,10 @@ def check_links(path):
             line_number += 1
             for match in LINK_REGEX.finditer(line):
                 name, uri = match.groups()
-                valid, comment = check_link(uri, path)
-                if not valid:
+                code, comment = check_link(uri, path)
+                if code == TEST_ERROR:
                     errors += 1
-                print("\t[{}] line {: 3}: '{}' => {}. {}".format(pass_emoji(valid), line_number, name, uri, comment))
+                print("\t[{}] line {: 3}: '{}' => {}. {}".format(TEST_EMOJI[code], line_number, name, uri, comment))
     print("{} errors in '{}'".format(errors, path))
     return errors
 
@@ -83,8 +102,17 @@ def all_markdown_files(directory):
                 if path.endswith(".md"):
                     yield os.path.join(root, path)
 
+# Runs the checker on all markdown files in a directory
+# Return True if passing
 def run_all_checks():
-    return sum(check_links(path) for path in all_markdown_files("."))
+    checked = 0
+    passed = 0
+    for path in all_markdown_files("."):
+        if check_links(path) == 0:
+            passed += 1
+        checked += 1
+    print("{} files checked, {} passing.".format(checked, passed))
+    return checked == passed
 
 if __name__ == "__main__":
-    exit(1 if run_all_checks() > 0 else 0)
+    exit(0 if run_all_checks() else 1)
