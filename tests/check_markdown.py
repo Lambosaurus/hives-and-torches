0d522dcafe1ce5092f980484497da84d76e21244
Tim Lamborn <tlamborn@gmail.com>
2022-03-23 00:48:20 +1300

    markdown checker WIP

--- tests/check_markdown.py
+++ tests/check_markdown.py
@@ -13,7 +13,18 @@ def parse_ignore_file(path):
                 if key:
                     yield key
 
-IGNORE_URIS = [k for k in parse_ignore_file("tests/uri.ignore")]
+
+def parse_links_file(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding="utf8") as f:
+            for line in f.readlines():
+                key, uri = line.split("//")[0].strip().split(",")
+                if key:
+                    yield key, uri
+
+
+LINK_URIS = { k:v for k,v in parse_links_file("tests/links.txt") }
+IGNORE_URIS = [ k for k in parse_ignore_file("tests/uri.ignore") ]
 
 TEST_OK = 0
 TEST_WARN = 1
@@ -74,6 +85,33 @@ def check_link(uri, source_path):
     # Looks fine so far
     return TEST_OK, "No anchor."
 
+# Returns the relative uri of a path to another path
+def relative_uri(file, uri):
+
+    # split out the file and the anchor
+    if uri.count('#') == 0:
+        path, anchor = uri, None
+    else:
+        path, anchor = uri.split('#')
+
+    # If the path is the file, then we can just return the anchor
+    if path == file:
+        return "#" + anchor
+
+    # Otherwise we need to return the relative path
+    return os.path.relpath(path, os.path.dirname(file)) + "#" + anchor
+
+# Returns the appropriate link if it exists
+def find_link(path, name, uri):
+    if name in LINK_URIS:
+        candidate = relative_uri(path, LINK_URIS[name])
+    else:
+        candidate = relative_uri(path, uri)
+    code, comment = check_link(candidate, path)
+    if code == TEST_OK:
+        return candidate
+    return None
+
 # Iterates over all anchors in a markdown file
 def all_anchors(path):
     with open(path, 'r', encoding="utf8") as f:
@@ -83,21 +121,68 @@ def all_anchors(path):
                 yield match.group(1).strip()
 
 # Checks the contents of a markdown file for links
-def check_links(path, verbosity = TEST_OK):
+def check_links(path, verbosity = TEST_OK, fix_links = False):
     print("Scanning '{}':".format(path))
     errors = 0
     line_number = 0
+
+    if fix_links:
+        lines = []
+
     with open(path, 'r', encoding="utf8") as f:
         for line in f.readlines():
             line_number += 1
             for match in LINK_REGEX.finditer(line):
                 name, uri = match.groups()
                 code, comment = check_link(uri, path)
+
+                if code == TEST_ERROR:
+                    new_uri = find_link(path, name, uri)
+
+                    if new_uri is not None:
+
+                        if fix_links:
+                            line = line.replace(uri, new_uri, 1)
+                            code = TEST_WARN
+                            uri = new_uri
+                            comment = "Substituted link."
+                        else:
+                            comment = "Substitution available: \"{}\"".format(new_uri)
+
                 if code == TEST_ERROR:
                     errors += 1
+
                 line_ref = "{}:{}".format(path, line_number)
                 if code >= verbosity:
                     print("\t[{}] {} '{}' => {}. {}".format(TEST_EMOJI[code], line_ref, name, uri, comment))
+
+            if fix_links:
+                lines.append(line)
+
+    if fix_links:
+        with open(path, 'w', encoding="utf8", newline="\n") as f:
+            f.writelines(lines)
+
+    print("{} errors in '{}'".format(errors, path))
+    return errors
+
+# Rewrites the links in a markdown file with the appropriate links
+def fix_links(path, verbosity = TEST_OK):
+    print("Fixing '{}':".format(path))
+    with open(path, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+    errors = 0
+    line_number = 0
+    with open(path, 'w', encoding="utf8") as f:
+        for line in lines:
+            line_number += 1
+            for match in LINK_REGEX.finditer(line):
+                name, uri = match.groups()
+                new_uri = find_link(path, name, uri)
+                if new_uri is not None:
+                    line = line.replace(uri, new_uri)
+                    errors += 1
+            f.write(line)
     print("{} errors in '{}'".format(errors, path))
     return errors
 
@@ -110,11 +195,11 @@ def all_markdown_files(directory):
 
 # Runs the checker on all markdown files in a directory
 # Return True if passing
-def run_all_checks(verbosity):
+def run_all_checks(directory, verbosity, fix):
     checked = 0
     passed = 0
-    for path in all_markdown_files("."):
-        if check_links(path, verbosity) == 0:
+    for path in all_markdown_files(directory):
+        if check_links(path, verbosity, fix) == 0:
             passed += 1
         checked += 1
     print("{} files checked, {} passing.".format(checked, passed))
@@ -123,5 +208,7 @@ def run_all_checks(verbosity):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbosity", help="Level of errors to display. (0: All, 1: Warn, 2: Errors)", type=int, default=TEST_OK)
+    parser.add_argument("--directory", help="Directory to scan for markdown files.", default=".")
+    parser.add_argument("--fix", help="Fix links in markdown files.", action="store_true")
     args = parser.parse_args()
     exit(0 if run_all_checks(**vars(args)) else 1)
